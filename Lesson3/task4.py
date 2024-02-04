2535dc70729775f0287109ac2e0eed6d02177a77
skolinpy <skolinpy@gmail.com>
2024-02-04 20:27:18 +0300

    посчитать позитив негатив

--- Lesson3/task4.py
+++ Lesson3/task4.py
@@ -2,8 +2,8 @@ print("Введите три числа")
 a = input("число а=")
 b = input("число b=")
 c = input("число c=")
-txt = (a,b,c)
-
-
-
+positive = (int(a)>0)+(int(b)>0)+(int(c)>0)
+negative = (int(a)<0)+(int(b)<0)+(int(c)<0)
+zero = (int(a)==0)+(int(b)==0)+(int(c)==0)
+print(positive,negative,zero)
 
